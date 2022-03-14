45241fa6031d44b78804a66b616a0b648149b17b
bou24-1 <74868366+bou24-1@users.noreply.github.com>
2022-03-14 12:29:08 +0700

    Add files via upload

---
+++ Lab01/DrawLine.h
@@ -0,0 +1,40 @@
+
+// DrawLine.h : main header file for the DrawLine application
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"       // main symbols
+
+
+// DrawLineApp:
+// See DrawLine.cpp for the implementation of this class
+//
+
+class DrawLineApp : public CWinAppEx
+{
+public:
+	DrawLineApp() noexcept;
+
+
+	// Overrides
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+	// Implementation
+	UINT  m_nAppLook;
+	BOOL  m_bHiColorIcons;
+
+	virtual void PreLoadState();
+	virtual void LoadCustomState();
+	virtual void SaveCustomState();
+
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern DrawLineApp theApp;
